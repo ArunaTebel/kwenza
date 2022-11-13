d571d822543f16e1787947000dcdb0bfc83c23f8
ArunaTebel <arunatebel@hotmail.com>
2022-11-14 00:59:48 +0530

    Fix issue: check the availability of session before accession session.user

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -5,7 +5,7 @@ export async function load(event) {
     const session = await getServerSession(event);
     const {supabaseClient} = await getSupabase(event);
     let globalSettings;
-    if (session.user) {
+    if (session && session.user) {
         const profile = await getUserProfile(session.user.id, supabaseClient);
         globalSettings = profile.global_settings;
     }
